a8c1746fe81d0dd9b76f82f50c53d37c4e0d848e
daniel <dev.danielcw@gmail.com>
2023-07-01 23:23:02 -0500

    test commit

--- base.js
+++ base.js
@@ -9,9 +9,15 @@ function Person(name, age, haircolor, voter) {
 
 
 const person1 = new Person("Jim", 20, "blonde", true);
-
 const person2 = new Person("jane", 30, "brown", true);
-
 const person3 = new Person("billy", 14, "black", false);
 
-console.log(person1)
+
+Person.prototype.greeting = function() {
+    return `Hello, ${this.name}!`
+}
+// greeting prototype did not work when it was an arrow function
+
+console.log(person1.greeting())
+console.log(person2.greeting())
+console.log(person1, person2, person3)
